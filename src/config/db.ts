e41b881dcf20076f319f17287a1747200b539371
developerOmega <theskip98@gmail.com>
2021-09-12 17:53:40 -0500

    Agregar documentacion de estilos

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,9 +2,11 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, doc } from 'firebase/firestore/lite';
 import { firebaseConfig } from '../keys/firebace';
 
+// Inicializar la base de datos de Firestore
 const app = initializeApp(firebaseConfig);
 const db = getFirestore( app );
 
+// Buscar documentos en las colecciones de usuario
 const getUserColection = ( id:string ) => {
   return doc( db, 'users', id );
 }
